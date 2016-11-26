709c98c4269d2b838d1964bb098a855e7c5d2cb7
Coman Games <alfaa.gen@gmail.com>
2016-11-26 05:01:00 +0200

    Character import and recreation is done. Owl animation is ready

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	public void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public void Update () {
 	
 	}
 }
